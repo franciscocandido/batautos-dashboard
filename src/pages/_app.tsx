d22f8d9bddd9f81da18d42da1f451068b1e9f4f5
FranciscoCandido <franciscocandidolyrio@gmail.com>
2022-11-12 15:18:54 -0300

    :sparkles: (supabase): setup supabase auth helpers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { Session, SessionContextProvider } from '@supabase/auth-helpers-react';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
 import theme from '../lib/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionContextProvider>
   );
 }
